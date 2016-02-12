c54ebf41aa975197396f754e3d87d1ac843f48bd
Sarah Allen <sarah@zooniverse.org>
2016-02-12 11:33:31 -0600

    Start to webpack switch

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'app/main.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new CopyWebpackPlugin([
+      { from: 'assets' }
+    ])
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.cjsx', '.coffee', '.styl', '.css'],
+    modulesDirectories: ['.', 'node_modules']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }, {
+      test: /\.cjsx$/, 
+      exclude: /node_modules/,
+      loaders: ['coffee', 'cjsx']
+    }, { 
+      test: /\.coffee$/, 
+      loader: 'coffee' 
+    }, {
+      test: /\.json?$/,
+      loader: 'json'
+    }, {
+      test   : /\.css$/,
+      loaders: ['style', 'css']
+    }, { 
+      test: /\.styl$/, 
+      loader: 'style-loader!css-loader!stylus-loader' 
+    }, {
+      test: /\.(jpg|png|gif|otf|eot|svg|ttf|woff\d?)$/,
+      loader: 'file-loader'
+    }]
+  }
+};
